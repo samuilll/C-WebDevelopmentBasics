a7cae3a1645e182a05dcf9a5f181dc328750cc75
samuilll <samuil_dechev@abv.bg>
2018-10-06 13:42:50 +0300

    Database seeder added

---
+++ 04_IRunesApp/IRunesApp/Common/DatabaseSeeder.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IRunes.Domain.Models;
+using IRunesData;
+
+namespace IRunesApp.Common
+{
+   public class DatabaseSeeder
+    {
+        public void Seed(RunesDbContext db)
+        {
+            List<Album> albums = new List<Album>()
+                    {
+                        new Album()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "Master Of Puppets",
+                            Cover =
+                                "https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/Metallica_-_Master_of_Puppets_cover.jpg/220px-Metallica_-_Master_of_Puppets_cover.jpg"
+                        },
+                        new Album()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Name = "",
+                            Cover = "https://images.eil.com/large_image/QUEEN_PLAY%2BTHE%2BGAME-291209.jpg",
+                        }
+                    };
+
+            var firstAlbumTracks = new HashSet<AlbumTrack>()
+                    {
+                        new AlbumTrack()
+                        {
+                            AlbumId = albums[0].Id,
+                            Track = new Track()
+                            {
+                                Name = "Battery",
+                                Link = "https://www.youtube.com/embed/UipTt-qqZOE",
+                                Price = 4.56M
+                            }
+                        },
+                         new AlbumTrack()
+                        {
+                            AlbumId = albums[0].Id,
+                            Track = new Track()
+                            {
+                                Name = "Master of puppets",
+                                Link = "https://www.youtube.com/embed/xnKhsTXoKCI",
+                                Price = 6.56M
+                            }
+                        }, new AlbumTrack()
+                        {
+                            AlbumId = albums[0].Id,
+                            Track = new Track()
+                            {
+                                Name = "The thing that should not  be",
+                                Link = "https://www.youtube.com/embed/DuWtFk1Lue4",
+                                Price = 4.56M
+                            }
+                        },
+                        new AlbumTrack()
+                        {
+                            AlbumId = albums[0].Id,
+                            Track = new Track()
+                            {
+                                Name = "Sanataoritum",
+                                Link = "https://www.youtube.com/embed/WElvEZj0Ltw",
+                                Price = 4.56M
+                            }
+                        },
+                    };
+
+            var secondAlbumTracks = new HashSet<AlbumTrack>()
+                    {
+                        new AlbumTrack()
+                        {
+                            AlbumId = albums[1].Id,
+                            Track = new Track()
+                            {
+                                Name = "Play the game",
+                                Link = "https://www.youtube.com/embed/LS1RXZ6qpLc",
+                                Price = 4.56M
+                            }
+                        },
+                        new AlbumTrack()
+                        {
+                            AlbumId = albums[1].Id,
+                            Track = new Track()
+                            {
+                                Name = "Another one bites to dust",
+                                Link = "https://www.youtube.com/embed/rY0WxgSXdEE",
+                                Price = 4.56M
+                            }
+                        }, new AlbumTrack()
+                        {
+                            AlbumId = albums[1].Id,
+                            Track = new Track()
+                            {
+                                Name = "Spread your wings",
+                                Link = "https://www.youtube.com/embed/uyd6OLyhPJo",
+                                Price = 6.56M
+                            }
+                        }, new AlbumTrack()
+                        {
+                            AlbumId = albums[0].Id,
+                            Track = new Track()
+                            {
+                                Name = "The thing that should not  be",
+                                Link = "https://www.youtube.com/embed/DuWtFk1Lue4",
+                                Price = 4.56M
+                            }
+                        }
+                    };
+
+            db.Albums.AddRange(albums);
+            db.SaveChanges();
+        }
+    }
+}
--- 04_IRunesApp/IRunesApp/IRunesApplication.cs
+++ 04_IRunesApp/IRunesApp/IRunesApplication.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IRunes.Domain.Models;
+using IRunesApp.Common;
 using IRunesApp.Controllers;
 using IRunesData;
 using IRunesServices;
@@ -8,76 +12,81 @@ using SIS.WebServer.Routing.Contracts;
 
 namespace IRunesApp
 {
-    public class IRunesApplication:IApplication
+    public class IRunesApplication : IApplication
     {
         public void InitializeDatabase()
         {
             using (RunesDbContext db = new RunesDbContext())
             {
                 db.Database.Migrate();
-            }
-        }
 
-        public void Configure(IAppRouteConfig appRouteConfig)
-        {
-            appRouteConfig.AddAnonymousPath("/");
-
-            appRouteConfig.AddAnonymousPath("/Users/register");
-
-            appRouteConfig.AddAnonymousPath("/Users/login");
-
-            appRouteConfig.Get(
-                "/",
-                req=>new HomeController().Index(req));
-
-            appRouteConfig.Get(
-                "/Users/login",
-                req => new AccountController().Login(req.Session));
-
-            appRouteConfig.Post(
-                "/Users/login",
-                req => new AccountController().Login(req.Session,req.FormData));
-
-            appRouteConfig.Get(
-                "/Users/register",
-                req => new AccountController().Register(req.Session));
-
-            appRouteConfig.Post(
-                "/Users/register",
-                req => new AccountController().Register(req.Session,req.FormData));
-
-            appRouteConfig.Get(
-                "/Users/logout",
-                req => new AccountController().Logout(req.Session));
-
-            appRouteConfig.Get(
-                "/Albums/create",
-                req => new AlbumController().Create(req.Session));
-
-            appRouteConfig.Post(
-                "/Albums/create",
-                req => new AlbumController().Create(req.FormData));
-
-            appRouteConfig.Get(
-                "/Albums/all",
-                req => new AlbumController().All(req.Session));
-
-            appRouteConfig.Get(
-                "/Albums/details?albumId={(?<albumId>[A-Za-z0-9-]+)}",
-                req => new AlbumController().Details(req.Session,req.UrlParameters["albumId"]));
-
-            appRouteConfig.Get(
-                "/Tracks/create?albumId={(?<albumId>.+)}",
-                req => new TrackController().Create(req.UrlParameters));
-
-            appRouteConfig.Post(
-                "/Tracks/create?albumId={(?<albumId>.+)}",
-                req => new TrackController().Create(req));
-
-            appRouteConfig.Get(
-                "/Tracks/details?albumId={(?<albumId>[A-Za-z0-9-]+)}&trackId={(?<trackId>[A-Za-z0-9-]+)}}",
-                req => new TrackController().Details(req));
-
+                if (!db.Albums.Any())
+                {
+                   new DatabaseSeeder().Seed(db);
+                }
         }
     }
+
+    public void Configure(IAppRouteConfig appRouteConfig)
+    {
+        appRouteConfig.AddAnonymousPath("/");
+
+        appRouteConfig.AddAnonymousPath("/Users/register");
+
+        appRouteConfig.AddAnonymousPath("/Users/login");
+
+        appRouteConfig.Get(
+            "/",
+            req => new HomeController().Index(req));
+
+        appRouteConfig.Get(
+            "/Users/login",
+            req => new AccountController().Login(req.Session));
+
+        appRouteConfig.Post(
+            "/Users/login",
+            req => new AccountController().Login(req.Session, req.FormData));
+
+        appRouteConfig.Get(
+            "/Users/register",
+            req => new AccountController().Register(req.Session));
+
+        appRouteConfig.Post(
+            "/Users/register",
+            req => new AccountController().Register(req.Session, req.FormData));
+
+        appRouteConfig.Get(
+            "/Users/logout",
+            req => new AccountController().Logout(req.Session));
+
+        appRouteConfig.Get(
+            "/Albums/create",
+            req => new AlbumController().Create(req.Session));
+
+        appRouteConfig.Post(
+            "/Albums/create",
+            req => new AlbumController().Create(req.FormData));
+
+        appRouteConfig.Get(
+            "/Albums/all",
+            req => new AlbumController().All(req.Session));
+
+        appRouteConfig.Get(
+            "/Albums/details?albumId={(?<albumId>[A-Za-z0-9-]+)}",
+            req => new AlbumController().Details(req.Session, req.UrlParameters["albumId"]));
+
+        appRouteConfig.Get(
+            "/Tracks/create?albumId={(?<albumId>.+)}",
+            req => new TrackController().Create(req.UrlParameters));
+
+        appRouteConfig.Post(
+            "/Tracks/create?albumId={(?<albumId>.+)}",
+            req => new TrackController().Create(req));
+
+        appRouteConfig.Get(
+            "/Tracks/details?albumId={(?<albumId>[A-Za-z0-9-]+)}&trackId={(?<trackId>[A-Za-z0-9-]+)}}",
+            req => new TrackController().Details(req));
+
+    }
+}
 }
